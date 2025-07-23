7754348d6cf08353c1cb55b57b8efce50361ce21
J-KOEL <jaysonkoel05@gmail.com>
2025-07-23 16:16:32 -0500

    Update incandescent_pushpull.py

--- pages/incandescent_pushpull.py
+++ pages/incandescent_pushpull.py
@@ -1,30 +1,37 @@
-
 from utils import load_csv_dict
 
 def load_data():
-  operator_lookup = load_csv_dict("PushPullOperator.csv")
-  light_unit_lookup = load_csv_dict("IlluminatedPushPullIncandescentLightUnit.csv")
-  lens_lookup = load_csv_dict("IlluminatedPushPullIncandescentLens.csv")
-  circuit_lookup = load_csv_dict("Circuit.csv")
-  return operator_lookup, light_unit_lookup, lens_color_lookup, circuit_lookup
+    operator_lookup = load_csv_dict("PushPullOperator 7.csv")
+    light_unit_lookup = load_csv_dict("IlluminatedPushPullIncandescentLightUnit 4.csv")
+    lens_lookup = load_csv_dict("IlluminatedPushPullIncandescentLens 4.csv")
+    circuit_lookup = load_csv_dict("Circuit 18.csv")
+    return operator_lookup, light_unit_lookup, lens_lookup, circuit_lookup
 
-def decode(catalog_number, operator_lookup, light_unit_lookup, lens_color_lookup, circuit_lookup):
-  normailized = calalog_number.replace("-", "").strip().upper()
-  if normalized.startswith("10250T") and len(normalized) > 9:
-      code_part = normalized[6:]
-      operator_code = code_part [:1]
-      light_unit_code = code part [1:3]
-      lens_code = code part [3:5]
-      circuit_code = code part [5:]
+def decode(catalog_number, operator_lookup, light_unit_lookup, lens_lookup, circuit_lookup):
+    normalized = catalog_number.replace("-", "").strip().upper()
 
-      return {
-          "operator": operator_lookup.get(operator_code, "Unknown Operator"),
-          "Light Unit": light_unit_lookup.get(light_unit_code, "Unknown Light Unit"),
-          "Lens Color": lens_color_lookup.get(lens_code, "Unknown Lens Color"),
-          "Circuit Type": circuit_lookup.get(circuit_code, "Unknown Circuit Type"),
-          "Light Unit P/N": f"10250T{light_unit_code}",
-          "Lens P/N": lens_code,
-          "Voltage Code": voltage_code,
-          "Contact Block P/N": f"10250T{circuit_code}"
-      }
-    return None
+    if not normalized.startswith("10250T") or len(normalized) < 11:
+        return None
+
+    code_part = normalized[6:]
+
+    operator_code = code_part[:1]
+    light_unit_code = code_part[1:3]
+    lens_code = code_part[3:6]
+    circuit_code = code_part[6:]
+
+    labels = {
+        "Operator Type": operator_lookup.get(operator_code, "Unknown Operator Code"),
+        "Light Unit": light_unit_lookup.get(light_unit_code, "Unknown Light Unit"),
+        "Lens": lens_lookup.get(lens_code, "Unknown Lens Code"),
+        "Circuit Type": circuit_lookup.get(circuit_code, "Unknown Circuit Code")
+    }
+
+    parts = {
+        "Operator P/N": f"10250T{operator_code}",
+        "Light Unit P/N": light_unit_code,
+        "Lens P/N": lens_code,
+        "Contact Block P/N": f"10250T{circuit_code}"
+    }
+
+    return {"labels": labels, "parts": parts}
