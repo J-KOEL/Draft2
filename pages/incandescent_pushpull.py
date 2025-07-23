0362a5a31244965277306818604fb6bbec30ee6b
J-KOEL <jaysonkoel05@gmail.com>
2025-07-23 16:03:26 -0500

    Update incandescent_pushpull.py

--- pages/incandescent_pushpull.py
+++ pages/incandescent_pushpull.py
@@ -4,7 +4,7 @@ from utils import load_csv_dict
 def load_data():
   operator_lookup = load_csv_dict("PushPullOperator.csv")
   light_unit_lookup = load_csv_dict("IlluminatedPushPullIncandescentLightUnit.csv")
-  lens_color_lookup = load_csv_dict("IlluminatedPushPullIncandescentLens.csv")
+  lens_lookup = load_csv_dict("IlluminatedPushPullIncandescentLens.csv")
   circuit_lookup = load_csv_dict("Circuit.csv")
   return operator_lookup, light_unit_lookup, lens_color_lookup, circuit_lookup
 
@@ -14,16 +14,16 @@ def decode(catalog_number, operator_lookup, light_unit_lookup, lens_color_lookup
       code_part = normalized[6:]
       operator_code = code_part [:1]
       light_unit_code = code part [1:3]
-      lens_color_code = code part [3:5]
+      lens_code = code part [3:5]
       circuit_code = code part [5:]
 
       return {
           "operator": operator_lookup.get(operator_code, "Unknown Operator"),
           "Light Unit": light_unit_lookup.get(light_unit_code, "Unknown Light Unit"),
-          "Lens Color": lens_color_lookup.get(lens_color_code, "Unknown Lens Color"),
+          "Lens Color": lens_color_lookup.get(lens_code, "Unknown Lens Color"),
           "Circuit Type": circuit_lookup.get(circuit_code, "Unknown Circuit Type"),
           "Light Unit P/N": f"10250T{light_unit_code}",
-          "Lens P/N": lens_color_code,
+          "Lens P/N": lens_code,
           "Voltage Code": voltage_code,
           "Contact Block P/N": f"10250T{circuit_code}"
       }
