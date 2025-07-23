eeef14f1cd6658fe7fd49a5f7d59197ecdc952e5
J-KOEL <jaysonkoel05@gmail.com>
2025-07-23 14:48:10 -0500

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,10 @@
+
+import pandas as pd
+
+def load_csv_dict(file_path, key_col="Code", value_col="Label"):
+    df = pd.read_csv(file_path)
+    return {str(row[key_col]).strip(): str(row[value_col]).strip() for _, row in df.iterrows()}
+
+def load_alternate_map(file_path):
+    df = pd.read_csv(file_path)
+    return {str(row["Alternate"]).strip().upper(): str(row["Standard"]).strip().upper() for _, row in df.iterrows()}
