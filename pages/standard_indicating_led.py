36851b3bf463364ef09213cedc029340c0e0a128
J-KOEL <jaysonkoel05@gmail.com>
2025-08-06 10:55:44 -0500

    Update standard_indicating_led.py

--- pages/standard_indicating_led.py
+++ pages/standard_indicating_led.py
@@ -2,7 +2,7 @@ from utils import load_csv_dict
 
 def load_data():
     light_unit_lookup = load_csv_dict("StandardindicatingLightLEDlightUnit.csv")
-    lens_lookup = load_csv_dict("StandardIndicatingLightLEDLens.csv")
+    lens_lookup = load_csv_dict("LEDIndicatingLightLensreference.csv")
     voltage_lookup = load_csv_dict("IndicatinglightLEDvoltage.csv")
     return light_unit_lookup, lens_lookup, voltage_lookup
 
@@ -14,12 +14,13 @@ def decode(catalog_number, light_unit_lookup, lens_lookup, voltage_lookup):
         lens_code = code_part[4:6]
         voltage_code = code_part[6:]
 
+    lens_info = lens_lookup.get(lens_code, {})
+
         return {
             "Light Unit": light_unit_lookup.get(light_unit_code, "Unknown Light Unit"),
             "Lens Color": lens_lookup.get(lens_code, "Unknown Lens Color"),
             "Voltage": voltage_lookup.get(voltage_code, "Unknown Voltage"),
             "Light Unit P/N": f"10250T{light_unit_code}",
-            "Lens P/N": f"10250T{lens_code}",
-            "Voltage P/N": f"10250T{voltage_code}"
+            "Lens P/N": lens_info.get("PartNumber", Unknown Lens P/N)
         }
     return None
